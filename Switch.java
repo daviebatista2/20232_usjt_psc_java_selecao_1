7f01e2e4025caeb4521331a0f8098bc39771cff5
Davi Batista <davideosmar11@gmail.com>
2023-09-11 20:23:17 -0300

    ap03(selecao): indentação

--- Switch.java
+++ Switch.java
@@ -2,27 +2,28 @@ import javax.swing.JOptionPane;
 
 public class Switch {
     public static void main (String[] args) {
-        var grade = "";
-        
-            grade = JOptionPane.showInputDialog("Digite a sua nota!");
-            switch(grade)   {
-                case "1":
-                    JOptionPane.showMessageDialog(null, "Oh não! Faremos melhor na próxima :(");
-                    break;
-                case "2":
-                    JOptionPane.showMessageDialog(null, "Dê seu feedback para que possamos melhorar. :(");
-                    break;
-                case "3":
-                    JOptionPane.showMessageDialog(null, "Obrigado pelo feedback! Esperamos atendê-lo melhor na próxima!");
-                    break;
-                case "4":
-                    JOptionPane.showMessageDialog(null, "Ficamos felizes que gostou! Volte sempre!");
-                    break;
-                case "5":
-                    JOptionPane.showMessageDialog(null, "Muito obrigado! Ficamos contentes que gostou de nosso serviço!");
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Insira uma nota válida!");
+        var grade = JOptionPane.showInputDialog("Digite a sua nota!");
+        switch(grade)   {
+            case "1":
+                JOptionPane.showMessageDialog(null, "Oh não! Faremos melhor na próxima :(");
+                break;
+            case "2":
+                JOptionPane.showMessageDialog(null, "Dê seu feedback para que possamos melhorar. :(");
+                break;
+            case "3":
+                JOptionPane.showMessageDialog(null, "Obrigado pelo feedback! Esperamos atendê-lo melhor na próxima!");
+                break;
+            case "4":
+                JOptionPane.showMessageDialog(null, "Ficamos felizes que gostou! Volte sempre!");
+                break;
+            case "5":
+                JOptionPane.showMessageDialog(null, "Muito obrigado! Ficamos contentes que gostou de nosso serviço!");
+                break;
+            case "0":
+                JOptionPane.showMessageDialog(null, "Encerrando...");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Insira uma nota válida!");
         }
     }
 }
