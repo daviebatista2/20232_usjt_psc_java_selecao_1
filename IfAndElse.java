fc1e30ba12ea0a523170625df9e95f08e312b270
Davi Batista <davideosmar11@gmail.com>
2023-09-11 21:08:37 -0300

    ap03(selecao): fim do exemplo if

--- IfAndElse.java
+++ IfAndElse.java
@@ -3,12 +3,8 @@ import javax.swing.JOptionPane;
 public class IfAndElse   {
     public static void main (String[] args) {
         double grade = Double.parseDouble(JOptionPane.showInputDialog("Digite a sua nota!"));
+        String message = grade < 70 ?  "Reprovado!" : "Aprovado!";
 
-        if  (grade < 70) {
-            JOptionPane.showMessageDialog(null, "Reprovado!");
-        }
-        else {
-            JOptionPane.showMessageDialog(null, "Aprovado!");
-        };
+        JOptionPane.showMessageDialog(null, message);
     }
 }
